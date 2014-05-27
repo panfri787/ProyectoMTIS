2043170a8c7073baa24a608be6835137bf47207c
elmesa <elmesamola@gmail.com>
2014-05-27 03:08:43 +0200

    Included FireEmployee AJAX response in XML or JSON by Accept header

--- Empresa1/src/main/webapp/js/Empresa1.js
+++ Empresa1/src/main/webapp/js/Empresa1.js
@@ -5,19 +5,19 @@
 Empresa1 = function Empresa1() {
 
 	this.fireEmployeeReport = new FireEmployeeReport();
-	
+
 	this.businessId = "Empresa1";
 
 	this.api = {}; // Data about de REST-API. Host, collection, url, etc
 	this.employees = []; // Array of employees of the business
 
 	this.init = function init() {
-		
+
 		this.fireEmployeeReport.init();
 
 		_.bindAll(this, "getEmployees");
 		_.bindAll(this, "onGetEmployeesResponse");
-		
+
 		_.bindAll(this, "getFireEmployeeReports");
 
 		_.bindAll(this, "drawEmployees");
@@ -29,7 +29,8 @@ Empresa1 = function Empresa1() {
 		_.bindAll(this, "fireEmployeeAJAXXML");
 
 		_.bindAll(this, "onfireEmployeeAJAXSuccess");
-		
+		_.bindAll(this, "onfireEmployeeSuccessXMLResponse");
+
 		_.bindAll(this, "onAjaxError");
 		_.bindAll(this, "onAjaxSuccess");
 
@@ -68,7 +69,7 @@ Empresa1 = function Empresa1() {
 	this.getFireEmployeeReports = function getFireEmployeeReports() {
 
 		console.log("Empresa1.getFireEmployeeReports()");
-		
+
 		this.api.collection = "isOkAndFireEmployeeReports";
 
 		var resourceUrl = this.api.url();
@@ -77,9 +78,10 @@ Empresa1 = function Empresa1() {
 			url : resourceUrl,
 			type : "GET",
 			dataType : 'json',
-		}).done(this.fireEmployeeReport.onGetCollectionResponse).fail(this.onAjaxError);
+		}).done(this.fireEmployeeReport.onGetCollectionResponse).fail(
+				this.onAjaxError);
 	};
-	
+
 	this.onAjaxError = function onAjaxError(xhr, status, error) {
 		console
 				.log(
@@ -118,8 +120,13 @@ Empresa1 = function Empresa1() {
 						+ employeeName
 						+ '</td><td>blablabla</td><td><button class="btn btn-danger" onclick="javascript:empresa1.fireEmployee('
 						+ employeeDNI
-						+ ')">REST-JSON</button><button class="btn btn-danger" onclick="javascript:empresa1.fireEmployeeRestXML('
-						+ employeeDNI + ')">REST-XML</button><button class="btn btn-danger" onclick="javascript:empresa1.fireEmployeeJms('
+						+ ')">REST-JSON-JSON</button><button class="btn btn-danger" onclick="javascript:empresa1.fireEmployeeREST_JSON_XML('
+						+ employeeDNI
+						+ ')">REST_JSON_XML</button><button class="btn btn-danger" onclick="javascript:empresa1.fireEmployeeRestXML('
+						+ employeeDNI
+						+ ')">REST-XML-JSON</button><button class="btn btn-danger" onclick="javascript:empresa1.fireEmployeeREST_XML_XML('
+						+ employeeDNI
+						+ ')">REST-XML-XML</button><button class="btn btn-danger" onclick="javascript:empresa1.fireEmployeeJms('
 						+ employeeDNI + ')">JMS-JSON</button></td></tr>';
 			}
 		}
@@ -139,7 +146,79 @@ Empresa1 = function Empresa1() {
 
 		this.fireEmployeeAJAX(dataJSON);
 	};
-	
+
+	this.fireEmployeeAJAX = function fireEmployeeAJAX(data) {
+		var proxyData = {
+			url : 'http://localhost:8081/FireEmployee',
+			data : data,
+			contentType : 'application/json',
+			accept : 'application/json',
+		};
+
+		$.ajax({
+			url : 'ProxyServlet',
+			type : 'POST',
+			dataType : 'json',
+			data : proxyData,
+		}).done(this.onfireEmployeeAJAXSuccess).fail(this.onAjaxError).always(
+				function() {
+					console.log("fireEmployeeAJAX() - Complete");
+				});
+	};
+
+	this.onfireEmployeeAJAXSuccess = function onAjaxSuccess(data, status, xhr) {
+		console
+				.log(
+						"Empresa1.onfireEmployeeAJAXSuccess(data %O, status %O, xhr %O)",
+						data, status, xhr);
+
+		this.fireEmployeeReport.addToCollection(data);
+	};
+
+	this.fireEmployeeREST_JSON_XML = function fireEmployeeREST_JSON_XML(
+			DNInumber) {
+
+		console.log("Empresa1.fireEmployeeREST_JSON_XML(DNInumber %O)",
+				DNInumber);
+		var data = {
+			dniId : DNInumber,
+			businessId : this.businessId
+		};
+
+		var dataJSON = JSON.stringify(data);
+
+		var proxyData = {
+			url : 'http://localhost:8081/FireEmployee',
+			data : dataJSON,
+			contentType : 'application/json',
+			accept : 'application/xml',
+		};
+
+		$.ajax({
+			url : 'ProxyServlet',
+			type : 'POST',
+			dataType : 'xml',
+			accept : 'application/xml',
+			data : proxyData,
+		//}).done(this.onfireEmployeeSuccessXMLResponse).fail(this.onAjaxError)
+		}).done(this.onAjaxSuccess).fail(this.onAjaxError)
+				.always(function() {
+					console.log("fireEmployeeAJAX() - Complete");
+				});
+	};
+
+	this.onfireEmployeeSuccessXMLResponse = function onAjaxSuccess(data,
+			status, xhr) {
+		console
+				.log(
+						"Empresa1.onfireEmployeeREST_JSON_XMLSuccess(data %O, status %O, xhr %O)",
+						data, status, xhr);
+
+		dataJSON = xmlToJson(data);
+
+		this.fireEmployeeReport.addToCollection(dataJSON);
+	};
+
 	this.fireEmployeeJms = function fireEmployeeJms(DNInumber) {
 
 		var data = {
@@ -157,43 +236,19 @@ Empresa1 = function Empresa1() {
 			url : 'api/fireEmployeeJMS',
 			type : 'POST',
 			dataType : 'json',
-			contentType: 'application/json',
+			contentType : 'application/json',
 			data : data,
 		}).done(this.onAjaxSuccess).fail(this.onAjaxError).always(function() {
 			console.log("fireEmployeeJmsAJAX() - Complete");
 		});
 	};
-	
-	this.fireEmployeeAJAX = function fireEmployeeAJAX(data) {
-		var proxyData = {
-			url : 'http://localhost:8081/FireEmployee',
-			data : data,
-			contentType : 'application/json',
-		};
-
-		$.ajax({
-			url : 'ProxyServlet',
-			type : 'POST',
-			dataType : 'json',
-			data : proxyData,
-		}).done(this.onfireEmployeeAJAXSuccess).fail(this.onAjaxError).always(function() {
-			console.log("fireEmployeeAJAX() - Complete");
-		});
-	};
-	
-	this.onfireEmployeeAJAXSuccess = function onAjaxSuccess(data, status, xhr) {
-		console.log("Empresa1.onfireEmployeeAJAXSuccess(data %O, status %O, xhr %O)", data,
-				status, xhr);
-		
-		this.fireEmployeeReport.addToCollection(data);
-	};
 
 	this.fireEmployeeRestXML = function fireEmployeeRestXML(DNInumber) {
-		
+
 		console.log("Empresa1.fireEmployeeRestXML(DNInumber %O)", DNInumber);
 
-		var data = '<DniIdAndBusinessId>' + '<dniId>'+ DNInumber +'</dniId>'
-				+ '<businessId>'+ this.businessId +'</businessId>'
+		var data = '<DniIdAndBusinessId>' + '<dniId>' + DNInumber + '</dniId>'
+				+ '<businessId>' + this.businessId + '</businessId>'
 				+ '</DniIdAndBusinessId>';
 
 		this.fireEmployeeAJAXXML(data);
@@ -204,6 +259,7 @@ Empresa1 = function Empresa1() {
 			url : 'http://localhost:8081/FireEmployee',
 			data : data,
 			contentType : 'application/xml',
+			accept : 'application/json',
 		};
 
 		$.ajax({
@@ -211,8 +267,77 @@ Empresa1 = function Empresa1() {
 			type : 'POST',
 			dataType : 'json',
 			data : proxyData,
-		}).done(this.onfireEmployeeAJAXSuccess).fail(this.onAjaxError).always(function() {
-			console.log("fireEmployeeAJAX() - Complete");
-		});
+		}).done(this.onfireEmployeeAJAXSuccess).fail(this.onAjaxError).always(
+				function() {
+					console.log("fireEmployeeAJAX() - Complete");
+				});
 	};
+
+	this.fireEmployeeREST_XML_XML = function fireEmployeeREST_XML_XML(DNInumber) {
+
+		console.log("Empresa1.fireEmployeeRestXML(DNInumber %O)", DNInumber);
+
+		var data = '<DniIdAndBusinessId>' + '<dniId>' + DNInumber + '</dniId>'
+				+ '<businessId>' + this.businessId + '</businessId>'
+				+ '</DniIdAndBusinessId>';
+
+		var proxyData = {
+			url : 'http://localhost:8081/FireEmployee',
+			data : data,
+			contentType : 'application/xml',
+			accept : 'application/xml',
+		};
+
+		$.ajax({
+			url : 'ProxyServlet',
+			type : 'POST',
+			dataType : 'json',
+			data : proxyData,
+		//}).done(this.onfireEmployeeSuccessXMLResponse).fail(this.onAjaxError).always( 
+		}).done(this.onAjaxSuccess).fail(this.onAjaxError).always(
+				function() {
+					console.log("fireEmployeeAJAX() - Complete");
+				});
+	};
+
+	
+};
+
+//Changes XML to JSON
+function xmlToJson(xml) {
+
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+			obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for (var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof (obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof (obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
 };
